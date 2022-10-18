d6d2f7903818076612c46569dd0070f353749193
Eugene Kotovich <sxekot@gmail.com>
2022-10-18 17:41:28 +0300

    Initial Commit

---
+++ Weather SUI/Weather_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SUIApp.swift
+//  Weather SUI
+//
+//  Created by Eugene Kotovich on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
